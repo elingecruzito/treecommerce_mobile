85c28ac6cf90b1d0a1ac963f52c7791cf9be460f
Andres Eleazar <andresandy0101@gmail.com>
2021-08-17 16:17:31 -0500

    Appbar Desing

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -6,7 +6,21 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        title: Center(child: Text('Home', style: TextStyle(color: Colors.white))),
+        leading: Container(),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.shopping_bag_outlined, color: Colors.white), 
+            onPressed: () => print('Shopping car!')
+          )
+        ],
+      ),
+      body: Column(
+        children: [
+
+        ],
+      ),
     );
   }
 }
