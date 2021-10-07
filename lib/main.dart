1f043757cd66bd8066f43dc3a76117571e59a47b
Andres Eleazar <andresandy0101@gmail.com>
2021-10-07 14:52:52 -0500

    List products widget created and update watched products

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,7 @@ import 'package:treecommerce/src/pages/error_page.dart';
 import 'package:treecommerce/src/pages/home_page.dart';
 import 'package:treecommerce/src/pages/launcher_page.dart';
 import 'package:treecommerce/src/pages/list_products_page.dart';
-import 'package:treecommerce/src/pages/list_watched_products_page.dart';
+import 'package:treecommerce/src/pages/watched_products_page.dart';
 import 'package:treecommerce/src/pages/login_page.dart';
 import 'package:treecommerce/src/pages/notifications_page.dart';
 import 'package:treecommerce/src/pages/search_page.dart';
@@ -41,7 +41,7 @@ class MyApp extends StatelessWidget {
           'notifications' : (BuildContext context) => NotificationsPage(),
           'buys' : (BuildContext context) => BuysPage(),
           'account' : (BuildContext context) => AccountPage(),
-          'watched' : (BuildContext context) => ListWatchedProductsPage(),
+          'watched' : (BuildContext context) => WatchedProductsPage(),
         },
         theme: ThemeData(
           primaryColor: global_color, 
--- lib/src/widgets/list_products_widget.dart
+++ lib/src/widgets/list_products_widget.dart
@@ -1,48 +1,19 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
-import 'package:treecommerce/src/model/galery_model.dart';
 import 'package:treecommerce/src/model/productos_model.dart';
-import 'package:treecommerce/src/provider/provider.dart';
-import 'package:treecommerce/src/services/galery_service.dart';
-import 'package:treecommerce/src/services/watched_service.dart';
-import 'package:treecommerce/src/utilerias/user_preferences.dart';
 import 'package:treecommerce/src/utilerias/utils.dart';
-import 'package:treecommerce/src/widgets/drawer_widget.dart';
 
-class ListWatchedProductsPage extends StatefulWidget {
-  ListWatchedProductsPage({Key key}) : super(key: key);
-
-  @override
-  _ListWatchedProductsPageState createState() => _ListWatchedProductsPageState();
-}
-
-class _ListWatchedProductsPageState extends State<ListWatchedProductsPage> {
+class ListProductsWidget extends StatelessWidget {
   
-  final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
-  WatchedService _service;
-  GaleryService _galeryService;
-  UserPreferences _userPreferences;
+  ListProductsWidget({ @required this.future });
+
+  Future<List<ProductosModel>> future;
 
   @override
   Widget build(BuildContext context) {
-    
-    _service = new WatchedService();
-    _galeryService = new GaleryService();
-    _userPreferences = Provider.userPreferences(context);
 
-    return Scaffold(
-      key: _scaffoldKey,
-      drawer: DrawerCustom(),
-      appBar: simpleAppBar(context, 'Historial'),
-      body: _listProducts(),
-    );
-  }
-
-  Widget _listProducts() {
     return Container(
       child: FutureBuilder(
-        future: _service.getWatchedList(_userPreferences),
+        future: this.future,
         builder: (BuildContext context, AsyncSnapshot<List<ProductosModel>> snapshot){
           if(!snapshot.hasData){
             return Center(
@@ -51,7 +22,7 @@ class _ListWatchedProductsPageState extends State<ListWatchedProductsPage> {
           }else{
             return ListView.builder(
               itemCount: snapshot.data.length,
-              itemBuilder: (context, index){
+              itemBuilder: (context, index) {
                 return _itemProduct(snapshot.data[index]);
               }
             );
@@ -61,7 +32,8 @@ class _ListWatchedProductsPageState extends State<ListWatchedProductsPage> {
     );
   }
 
-  Widget _itemProduct(ProductosModel data) {
+
+ Widget _itemProduct(ProductosModel data) {
 
     return Container(
       decoration: BoxDecoration(
